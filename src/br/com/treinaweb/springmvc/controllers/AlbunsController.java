68d10eba324b9627d58e1a3b31c858232acbf34d
Breno Souza <breno.vitoi@gmail.com>
2020-11-23 14:33:21 -0300

    Configurando os beans necessarios, Spring Data Repository Pattern

--- src/br/com/treinaweb/springmvc/controllers/AlbunsController.java
+++ src/br/com/treinaweb/springmvc/controllers/AlbunsController.java
@@ -1,7 +1,10 @@
 package br.com.treinaweb.springmvc.controllers;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,11 +12,24 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 import br.com.treinaweb.springmvc.dominios.Album;
+import br.com.treinaweb.springmvc.repositorios.RepositorioAlbum;
 
 @Controller
 @RequestMapping("/albuns")
 public class AlbunsController {
+	
+	@Autowired
+	private RepositorioAlbum repositorio; //cdi, inversão de controle
+	
+	@RequestMapping(value= "/listar", method = RequestMethod.GET)
+	public String listar(Model model) {
+		List<Album> albuns = repositorio.findAll();
+		model.addAttribute("albuns", albuns);
+		return "album.listar.tiles";
+	}
+	
 	
 	@RequestMapping(value = "/adicionar", method = RequestMethod.GET)
 	public String adicionar(Model model) {
@@ -26,8 +42,8 @@ public class AlbunsController {
 		if (result.hasErrors()) {
 			return "album.adicionar.tiles";
 		}
-		model.addAttribute("album", novoAlbum);
-		return "album.exibir.tiles";
+		repositorio.save(novoAlbum);
+		return "redirect:/albuns/listar";
 	}
 
 }
